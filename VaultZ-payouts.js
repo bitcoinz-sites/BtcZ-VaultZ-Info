30a8ffcae596d1502bc886b69c0cb7cc495a0828
MarcelusCH <buob.marcel@gmail.com>
2022-05-15 11:58:34 +0200

    Updated payouts

--- VaultZ-payouts.js
+++ VaultZ-payouts.js
@@ -9,22 +9,128 @@ module.exports = {
       'block': "",
       'status': ""
     },
+
+
+    {
+      'name': "Dev Cost Payout (2022 - 2/4)",
+      'description': "Gra (t1WKUkNhwAnyCLG2go28dncPUWLKqrX8bmB) 1.5M + 2.0M = 3.0M </br>"+
+                       "Nedbear (t1LinaMnKczSYZSpWvmN6QuyguVJBaWu34D) 1.5M </br>" +
+                       "Marcelus (t1d5VsEvwx69SU4czE5zcbDcF3KMk7j6g9s)  1.5M </br>" +
+                       "Cryptorex (t1YDBE15Tpqa2iG1iav22ijXduyUUxs1yos)  1.5M </br>",
+      'btcz': "8,000,000",
+      'txid': "fe8f66d8842fed5deede5e2d4776d100064edd91059cfdf33fe2cc52b9b84832",
+      'date': "May 6, 2022",
+      'block': "939769",
+      'status': "DONE"
+    },
+    {
+      'name': "Yearly rewards for the mining contest",
+      'description': "Arganized by Gra and Nedbear ",
+      'btcz': "2,000,000",
+      'txid': "1c567743b5ebafc18a2e40d76eb83d5bcac332896d09e408c910892e0969e8f7",
+      'date': "Mar 22, 2022",
+      'block': "914597",
+      'status': "DONE"
+    },
+
+
+    {
+      'name': "OTC sales",
+      'description': "Rok 10k usdt",
+      'btcz': "25,002900,700",
+      'txid': "19547295028ac849435f6fd62e227a354657ac87e60700e1d25ae84de74bd9bd",
+      'date': "Mar 2, 2022",
+      'block': "904307",
+      'status': "DONE"
+    },
+    {
+      'name': "Gra Dev Cost (2022 extra)",
+      'description': "",
+      'btcz': "1,000,000",
+      'txid': "efd683f97937030d4683b1d9d445b3bbaddaadd88d5fb29035933f19eb56299e",
+      'date': "Feb 16, 2022",
+      'block': "896954",
+      'status': "DONE"
+    },
+    {
+      'name': "Nedbear Dev Cost (2022 extra)",
+      'description': "",
+      'btcz': "1,000,000",
+      'txid': "6ef318cebf46248698facc1153b2f8facd9e417b09f9d05aefd23fdd8c987364",
+      'date': "Feb 16, 2022",
+      'block': "896972",
+      'status': "DONE"
+    },
+    {
+      'name': "Gra Dev Cost (2022 -1/4)",
+      'description': "",
+      'btcz': "1,500,000",
+      'txid': "e679eef92b3754612a3677e7458a02ef7e0d3dc597267edb94f68312e7d74878",
+      'date': "Jan 17, 2022",
+      'block': "881578",
+      'status': "DONE"
+    },
+    {
+      'name': "Cryptorex Dev Cost (2022 -1/4)",
+      'description': "",
+      'btcz': "1,500,000",
+      'txid': "5351e4dfbc48a1048ca5729a7373fa97db80af95b5a246e64f04965a3b1315f3",
+      'date': "Jan 17, 2022",
+      'block': "881664",
+      'status': "DONE"
+    },
+    {
+      'name': "Marcelus Dev Cost (2022 -1/4)",
+      'description': "",
+      'btcz': "1,500,000",
+      'txid': "9e917d067873e743586b66f364bf0dac62e6dc64f25a73d52981130fdfc87f6f",
+      'date': "Jan 17, 2022",
+      'block': "881623",
+      'status': "DONE"
+    },
+    {
+      'name': "Nedbear Dev Cost (2022 -1/4)",
+      'description': "",
+      'btcz': "1,500,000",
+      'txid': "43c5e936011f30ee30986d1d5a294bf69483f46d72ab64a268b5599c112f3516",
+      'date': "Jan 17, 2022",
+      'block': "881584",
+      'status': "DONE"
+    },
      {
-       'name': "The BtcZ Pong Game :-)",
-       'description': "This is a test batch... The data are only sample. A blabla that explain the project and how it was decided to found it with some aditional like inside like this <a href='https://marcelusch.github.io/BTCZ-RPC-DOC/'>RPC DOC</a>",
-       'btcz': "22 856 000",
-       'txid': "1eeba5d7ff29009280a1ef8e5f99e0b7d97f06ef8e829e1ead3291d5dcc42e33",
-       'date': "Apr 5, 2021 12:33:32 AM ",
-       'block': "729804",
+       'name': "Cryptorex .rocks Infrastructure 2020",
+       'description': "",
+       'btcz': "9,035,300",
+       'txid': "d19ec5cb3250796ced5ba7f47100a31012556615b6424c3bcd371376d2e1ecc1",
+       'date': "Apr 26, 2021",
+       'block': "741598",
        'status': "DONE"
      },
      {
-       'name': "Super Mario",
-       'description': "Again - This is a test batch... The data are only sample. A blabla that explain the project and how it was decided to found it with some aditional like inside like this <a href='https://marcelusch.github.io/BTCZ-RPC-DOC/'>RPC DOC</a>",
-       'btcz': "850 500",
-       'txid': "1eeba5d7ff29009280a1ef8e5f99e0b7d97f06ef8e829e1ead3291d5dcc42e33",
-       'date': "Apr 5, 2021 12:33:32 AM ",
-       'block': "729804",
+       'name': "Marcelus .app Infrastructure 2020",
+       'description': "",
+       'btcz': "8,003,058",
+       'txid': "784385a64077fba5198c71b964931d11aba9b9c17479eeae245baf43ea8b9a9a",
+       'date': "Apr 26, 2021",
+       'block': "741598",
+       'status': "DONE"
+     },
+     {
+       'name': "Rok RadioZ 2020",
+       'description': "",
+       'btcz': "4,460,303",
+       'txid': "0d5b8714053310103ab90d024dce1ba18633850eaec3458ef37f451d3197e05a",
+       'date': "Apr 26, 2021",
+       'block': "741598",
+       'status': "DONE"
+     },
+     {
+       'name': "VandarGR getBtcz.com 2020",
+       'description': "",
+       'btcz': "1,503,759",
+       'txid': "aeb38b31fabd6708a0223fd53b04a955d2a67e00734be18d4da629dbbd447a51",
+       'date': "Apr 26, 2021",
+       'block': "741598",
        'status': "DONE"
      },
 
